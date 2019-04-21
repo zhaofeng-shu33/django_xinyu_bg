5b4ae74466b29b6c2180f7f261583950552d3ece
zhaofeng-shu33 <616545598@qq.com>
2019-04-21 19:51:12 +0800

    make update profile possible

--- lawyer/serializer.py
+++ lawyer/serializer.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from rest_auth.serializers import UserDetailsSerializer
+from rest_framework.fields import empty
 from .models import Lawyer
+import pdb
 class LawyerDetailSerializer(serializers.ModelSerializer):
-    user = UserDetailsSerializer(many=False)
+    user = UserDetailsSerializer(many=False, required=False)
     class Meta:
         model = Lawyer
         fields = ('user', 'law_firm')
+    def update(self, instance, validated_data):
+        user_data = validated_data.get('user', None)
+        if(user_data):
+            user = instance.user
+            for k,v in user_data.items():
+                setattr(user, k, v)
+        for k,v in validated_data.items():
+            if(k == 'user'):
+                continue
+            setattr(instance, k, v)
+        instance.save()
+        return instance
+    def run_validation(self, data=empty):
+        # self the instance for UserSerializer
+        self.fields['user'].instance = self.instance.user
+        return super(LawyerDetailSerializer, self).run_validation(data=data)
