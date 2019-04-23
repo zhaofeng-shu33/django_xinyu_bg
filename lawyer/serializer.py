073609b0a84b62723e187bbacb99b78dfc05f936
zhaofeng-shu33 <616545598@qq.com>
2019-04-23 22:20:57 +0800

    update class list format

--- lawyer/serializer.py
+++ lawyer/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_auth.serializers import UserDetailsSerializer
 from rest_framework.fields import empty
-from .models import Lawyer, Class, School
+from .models import Lawyer, Class, School, Course
 import pdb
 class LawyerDetailSerializer(serializers.ModelSerializer):
     user = UserDetailsSerializer(many=False, required=False)
@@ -25,15 +25,20 @@ class LawyerDetailSerializer(serializers.ModelSerializer):
         # self the instance for UserSerializer
         self.fields['user'].instance = self.instance.user
         return super(LawyerDetailSerializer, self).run_validation(data=data)
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('name', 'grade')
 class ClassViewSerializer(serializers.ModelSerializer):
     school = serializers.StringRelatedField()
     lawyer = serializers.StringRelatedField()
+    course = CourseSerializer()
     class Meta:
         model = Class
-        fields = ('pk','school','lawyer','start_time')
+        fields = ('pk','school','lawyer','start_time', 'course', 'class_id')
         
 class SchoolSerializer(serializers.ModelSerializer):
-    classes = ClassViewSerializer(many=True)
+    classes = serializers.StringRelatedField()
     class Meta:
         model = School
         fields = ('pk','name','classes')
