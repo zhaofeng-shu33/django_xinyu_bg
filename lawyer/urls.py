d230dceb7ecf814aec024fa538111ee8aeb6244d
zhaofeng-shu33 <616545598@qq.com>
2019-04-24 21:08:41 +0800

    add lawyer apply class

--- lawyer/urls.py
+++ lawyer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import LawyerDetailsView, SchoolListView, ClassListView
+from .views import LawyerDetailsView, SchoolListView, ClassListView, ClassRegisterView
 
 urlpatterns = [
     path('lawyer/', LawyerDetailsView.as_view(), name='rest_lawyer_details'),
     path('school/', SchoolListView.as_view(), name='rest_school_list'),
-    path('class/', ClassListView.as_view(), name='rest_class_list')        
+    path('class/', ClassListView.as_view(), name='rest_class_list'),
+    path('class/<int:pk>', ClassRegisterView.as_view(), name='rest_class_register')
 ]
--- lawyer/views.py
+++ lawyer/views.py
@@ -1,7 +1,7 @@
-from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound
-from .serializer import LawyerDetailSerializer, SchoolSerializer, ClassViewSerializer
+from .serializer import LawyerDetailSerializer, SchoolSerializer, ClassViewSerializer, ClassApplySerializer
 from .models import Lawyer, Class, School
 # Create your views here.
 class LawyerDetailsView(RetrieveUpdateAPIView):
@@ -21,3 +21,14 @@ class SchoolListView(ListAPIView):
 class ClassListView(ListAPIView):
     queryset = Class.objects.order_by('-start_time')
     serializer_class = ClassViewSerializer
+    
+class ClassRegisterView(UpdateAPIView):
+    def get_object(self):
+        try:
+            return Lawyer.objects.get(user=self.request.user)
+        except Lawyer.DoesNotExist:
+            raise NotFound('not a lawyer')
+    permission_classes = (IsAuthenticated, )
+
+    queryset = Class.objects.all()
+    serializer_class = ClassApplySerializer
