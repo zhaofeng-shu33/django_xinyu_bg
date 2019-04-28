4b8e5e4575dcffa76db10f6506288be45979b67e
zhaofeng-shu33 <616545598@qq.com>
2019-04-28 12:44:58 +0800

    backend pagination

--- lawyer/views.py
+++ lawyer/views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from .serializer import LawyerDetailSerializer, ClassViewSerializer, ClassApplySerializer
 from .models import Lawyer, Class
-# Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 20
+
 class LawyerDetailsView(RetrieveUpdateAPIView):
     serializer_class = LawyerDetailSerializer
     permission_classes = (IsAuthenticated, )
@@ -16,7 +22,8 @@ class LawyerDetailsView(RetrieveUpdateAPIView):
             return p
 
 class ClassListView(ListAPIView):
-    queryset = Class.objects.order_by('-start_time')
+    queryset = Class.objects.all()
+    pagination_class = StandardResultsSetPagination
     serializer_class = ClassViewSerializer
     
 class ClassRegisterView(UpdateAPIView):
