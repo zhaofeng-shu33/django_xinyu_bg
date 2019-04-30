b9ff0b348032b6456280c2a8d9d1c5049962e47e
zhaofeng-shu33 <616545598@qq.com>
2019-04-30 10:50:46 +0800

    add lawyer office import support

--- lawyer/models.py
+++ lawyer/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class LawyerOffice(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
 class Lawyer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     law_firm = models.CharField(max_length=20, null=True)
+    office = models.ForeignKey(LawyerOffice, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return self.user.__str__()
 
