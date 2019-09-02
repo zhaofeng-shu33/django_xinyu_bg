d6564cd3a30a727d0f3068fa5b68fe1619844ee3
zhaofeng-shu33 <616545598@qq.com>
2019-09-02 12:32:15 +0800

    update test

--- lawyer/tests.py
+++ lawyer/tests.py
@@ -3,9 +3,7 @@ from django.contrib.auth.models import User
 from .models import Lawyer, Semester, initialize_empty_office, LawyerOffice, Semester
 from datetime import datetime
 # Create your tests here.
-
-class LawyerModelTests(TestCase):
-    def test_can_bind_lawyer_with_user(self):
+def create_necessary_test_data():
         # create current Semester
         s = Semester(name='2019', end_date=datetime.now())
         s.save()
@@ -19,9 +17,11 @@ class LawyerModelTests(TestCase):
         p = Lawyer(user=u)
         p.save()
         initialize_empty_office(p)
-        # assert
-        self.assertIs(p.user.username, 'testuser')
-        
+
+class LawyerModelTests(TestCase):
+    def test_can_bind_lawyer_with_user(self):
+        create_necessary_test_data()
+        p = Lawyer.objects.get(user__username='testuser')        
         s = Semester.objects.get_current()
         office_semester = p.lawyer_office_semesters.get(semester__id = s['id'])
         self.assertTrue(office_semester.office.name == '空')
